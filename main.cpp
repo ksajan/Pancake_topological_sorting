ebd0f9f0ca16487768ba0bbb0e1c0d9abac50d69
ksajan <ksajan12@gmail.com>
2021-05-03 01:47:07 +0530

    added sorting method

--- main.cpp
+++ main.cpp
@@ -6,10 +6,9 @@ using namespace std;
 struct Edge {
     int src, dest;
 };
-
 class Graph{
     public:
-    vector<vector<int>> adjacentList;
+    vector<vector<int> > adjacentList;
     Graph(vector<Edge> const &edgeList, int N){
         adjacentList.resize(N);
 
@@ -19,7 +18,7 @@ class Graph{
     }
 };
 
-int DFS(Graph const &graph, int v, vector<bool> &visited, vector<int> &start, vector<int> &finish, int &time){
+void DFS(Graph const &graph, int v, vector<bool> &visited, vector<int> &start, vector<int> &finish, int &time){
     start[v] = ++time; // Setting the starting time of vertex v
     visited[v] = true; //Marked node v as visited
 
@@ -33,12 +32,18 @@ int DFS(Graph const &graph, int v, vector<bool> &visited, vector<int> &start, ve
 
 }
 
-bool sortByDeparture(const vector<int> &v1, const vector<int> &v2){
-    return v[1] < v2[1];
+bool sortByFinishTime(const vector<int> &v1, const vector<int> &v2){
+    return v1[2] > v2[2];
 }
 
-void TopologicalSort(){
-    vector<vector<int> > array
+void topologicalSort(vector<vector<int> > &finishOrder, int rows, int cols){
+    sort(finishOrder.begin(), finishOrder.end(), sortByFinishTime);
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<cols ;j++)
+            cout << finishOrder[i][j] << " ";
+        cout << endl;
+    }
 }
 
 int main(){
@@ -58,11 +63,17 @@ int main(){
             DFS(graph, i, visited, start, finish, time);
         }
     }
-
+    vector<vector<int> > finishOrder(N);
     for (int i = 0; i < N; i++)
     {
         cout << "Vertex " << i << " (" << start[i] << ", " << finish[i] << ")" << endl;
-        vector<Edge>
+        finishOrder[i] = vector<int>(3);
+        finishOrder[i][0] = i;
+        finishOrder[i][1] = start[i];
+        finishOrder[i][2] = finish[i];
+
     }
+
+    topologicalSort(finishOrder, N, 3);
     return 0;
 }
